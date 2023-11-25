8d6a6a60bfbe285959e2b0588c810e4679acdce1
sayyestome <152028911+sayyestome@users.noreply.github.com>
2023-11-26 00:37:49 +0530

    Update script.js
    add click listener for no button

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
+
+noBtn.addEventListener("click", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
